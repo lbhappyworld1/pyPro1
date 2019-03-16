5670f4c1f7671a1f815909998aaebe9dc0733be8
lbhappyworld1 <103818443@qq.com>
2019-03-16 05:49:01 +0000

    Update test.py

--- test.py
+++ test.py
@@ -1,1 +1,19 @@
-print("hello world")
+import numpy as np
+from keras.datasets import mnist
+
+from keras.models import Sequential
+
+from keras.layers import Dense,Dropout,Flatten
+
+from keras.layers import Conv2D,MaxPooling2D
+
+#os.chdir("/sdcard/qpython/tmp")
+
+#model_dir="/tmp/"
+
+(X_train,Y_train),(X_test,Y_test) = mnist.load_data()
+
+print(X_train[0].shape)
+
+print(Y_train[0])
+
