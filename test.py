a32ad7708bb9e5e048744a9f69eb10c0c197aec1
lbhappyworld1 <103818443@qq.com>
2019-03-18 02:18:03 +0000

    Update test.py

--- test.py
+++ test.py
@@ -1,11 +1,12 @@
 #test py keras
 import numpy as np
+import tensorflow
 from tensorflow import keras
 from keras.datasets import mnist
-from keras.models import Sequential
-from keras.layers import Dense,Dropout,Flatten
-from keras.layers import Conv2D,MaxPooling2D
-(X_train,Y_train),(X_test,Y_test) = mnist.load_data()
+#from keras.models import Sequential
+#from keras.layers import Dense,Dropout,Flatten
+#from keras.layers import Conv2D,MaxPooling2D
+(X_train,Y_train),(X_test,Y_test) = keras.datasets.mnist.load_data()
 print(X_train[0].shape)
 print(Y_train[0])
 
