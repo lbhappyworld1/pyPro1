e7a5ba716cb9a63895d096a718379194e89f6bff
lbhappyworld1 <103818443@qq.com>
2019-03-18 02:20:24 +0000

    Update test.py

--- test.py
+++ test.py
@@ -2,7 +2,7 @@
 import numpy as np
 import tensorflow
 from tensorflow import keras
-from keras.datasets import mnist
+#from keras.datasets import mnist
 #from keras.models import Sequential
 #from keras.layers import Dense,Dropout,Flatten
 #from keras.layers import Conv2D,MaxPooling2D
